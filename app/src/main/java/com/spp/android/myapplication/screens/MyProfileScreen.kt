67aa88009395ba3c637d5d45742d89ca37bc763e
AskaV <caya2321@gmail.com>
2025-03-08 14:51:47 +0100

    adding changes to task 1 part 2
    Added values ​​to the file with sizes + arranged and logically arranged them
    Changed the name of the xml file to a more appropriate one
    Added colors for the theme
    Removed the nested component in the xml file
    Added a screen config for previewing work with compose
    Fixed the numerical and color values ​​in the kotlin files by replacing them with the appropriate values
    
    (cherry picked from commit 2a5ad9889030330ed8c0bb22f17ffb7f6920a300)

--- app/src/main/java/com/spp/android/myapplication/screens/MyProfileScreen.kt
+++ app/src/main/java/com/spp/android/myapplication/screens/MyProfileScreen.kt
@@ -9,21 +9,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.spp.android.myapplication.R
-import com.spp.android.myapplication.ui.components.SocialButton
-import com.spp.android.myapplication.ui.components.CustomOrangeButton
-import com.spp.android.myapplication.ui.components.EditProfileButton
+import com.spp.android.myapplication.SocialButton
+import com.spp.android.myapplication.CustomOrangeButton
+import com.spp.android.myapplication.EditProfileButton
+import com.spp.android.myapplication.ui.preview.PreviewConfig
+import com.spp.android.myapplication.ui.theme.*
+
 
 @Preview(
     showBackground = true,
     name = "FigmaMobileSize",
-    device = "spec:width=360px,height=720px,dpi=160"
+    device = "spec:width=${PreviewConfig.SCREEN_WIDTH}px,height=${PreviewConfig.SCREEN_HEIGHT}px,dpi=${PreviewConfig.SCREEN_DPI}"
+
 )
 @Composable
 fun MyProfileScreenPreview() {
@@ -31,6 +34,7 @@ fun MyProfileScreenPreview() {
         MyProfileScreen()
     }
 }
+
 
 @Composable
 fun MyProfileScreen(modifier: Modifier = Modifier) {
@@ -41,47 +45,50 @@ fun MyProfileScreen(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.5f)
-                .background(colorResource(id = R.color.blue))
+                .background(Blue)
         ) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(horizontal = 16.dp),
+                    .padding(horizontal = dimensionResource(id = R.dimen.default_padding)),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 16.dp, top = 24.dp),
+                        .padding(
+                            start = dimensionResource(id = R.dimen.default_padding),
+                            top = dimensionResource(id = R.dimen.margin_top_settings)
+                        ),
                     contentAlignment = Alignment.TopStart
                 ) {
                     Text(
                         text = stringResource(R.string.settings),
                         style = MaterialTheme.typography.headlineLarge,
-                        color = colorResource(id = R.color.white)
+                        color = White
                     )
                 }
 
-                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.default_spacing)))
 
                 ProfileAvatar()
 
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.small_spacing)))
 
                 Text(
                     text = stringResource(R.string.user_name),
                     style = MaterialTheme.typography.headlineMedium,
-                    color = colorResource(id = R.color.white)
+                    color = White
                 )
                 Text(
                     text = stringResource(R.string.user_profession),
                     style = MaterialTheme.typography.titleMedium,
-                    color = colorResource(id = R.color.grayText2)
+                    color = GrayText2
                 )
                 Text(
                     text = stringResource(R.string.user_address),
                     style = MaterialTheme.typography.titleMedium,
-                    color = colorResource(id = R.color.grayText2)
+                    color = GrayText2
                 )
             }
         }
@@ -89,18 +96,29 @@ fun MyProfileScreen(modifier: Modifier = Modifier) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.White),
+                .background(White),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.weight(1f / 3f))
 
             Row(
-                horizontalArrangement = Arrangement.spacedBy(24.dp),
+                horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.social_button_spacing)),
                 modifier = Modifier.padding(top = 0.dp)
             ) {
-                SocialButton(R.drawable.social_facebook, stringResource(R.string.facebook))
-                SocialButton(R.drawable.social_instagram, stringResource(R.string.instagram))
-                SocialButton(R.drawable.social_telegram, stringResource(R.string.telegram))
+                SocialButton(
+                    iconRes = R.drawable.social_facebook,
+                    contentDescription = stringResource(R.string.facebook)
+                )
+
+                SocialButton(
+                    iconRes = R.drawable.social_instagram,
+                    contentDescription = stringResource(R.string.instagram)
+                )
+
+                SocialButton(
+                    iconRes = R.drawable.social_telegram,
+                    contentDescription = stringResource(R.string.telegram)
+                )
             }
 
             Spacer(modifier = Modifier.weight(2f / 3f))
@@ -116,7 +134,7 @@ fun ProfileAvatar() {
         painter = painterResource(id = R.drawable.baseline_account_circle_avatar),
         contentDescription = stringResource(R.string.user_avatar),
         modifier = Modifier
-            .size(113.dp)
+            .size(dimensionResource(id = R.dimen.profile_avatar_size))
             .clip(CircleShape)
     )
 }
@@ -125,12 +143,12 @@ fun ProfileAvatar() {
 fun ActionButtons() {
     Column(
         modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .padding(bottom = 16.dp)
+            .padding(horizontal = dimensionResource(id = R.dimen.default_padding))
+            .padding(bottom = dimensionResource(id = R.dimen.default_padding))
     ) {
         EditProfileButton(onClick = { /* TODO: Edit Profile Action */ })
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.default_spacing)))
 
         CustomOrangeButton(
             text = stringResource(R.string.view_contacts),
