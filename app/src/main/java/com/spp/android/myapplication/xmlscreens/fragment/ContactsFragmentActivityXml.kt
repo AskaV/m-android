1df11f361be7ac027b216c8b889cfd1dfbd857d8
AskaV <caya2321@gmail.com>
2025-07-12 22:42:19 +0200

    implemented navigation between contacts list and contact details

---
+++ app/src/main/java/com/spp/android/myapplication/xmlscreens/fragment/ContactsFragmentActivityXml.kt
@@ -0,0 +1,13 @@
+package com.spp.android.myapplication.xmlscreens.fragment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.spp.android.myapplication.R
+
+class ContactsFragmentActivityXml : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_xml_host)
+
+    }
+}
