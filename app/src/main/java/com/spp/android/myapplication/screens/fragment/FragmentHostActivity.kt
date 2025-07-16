e54c072cf58f22812a1870bab84a56c7f67362e6
AskaV <caya2321@gmail.com>
2025-07-16 05:45:52 +0200

    implemented navigation between contacts list and contact details for compose v.
    Update
    moved contacts list markup to a fragment
    
    created contact details fragment

---
+++ app/src/main/java/com/spp/android/myapplication/screens/fragment/FragmentHostActivity.kt
@@ -0,0 +1,35 @@
+package com.spp.android.myapplication.screens.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.fragment
+
+class FragmentHostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val container = FragmentContainerView(this).apply {
+            id = View.generateViewId()
+        }
+        setContentView(container)
+
+        val navHostFragment = NavHostFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(container.id, navHostFragment)
+            .setPrimaryNavigationFragment(navHostFragment)
+            .commitNow()
+
+        val navController = navHostFragment.navController
+        val navGraph = navController.createGraph(startDestination = "contacts") {
+            fragment<ContactsFragment>("contacts")
+            fragment<ContactDetailFragment>("contact_detail")
+        }
+
+        navController.graph = navGraph
+    }
+}
