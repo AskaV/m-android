c6f4580052b3cd704c96ae25890330bf31c124ba
AskaV <caya2321@gmail.com>
2025-08-13 05:33:23 +0200

    change files for correct work of compose branch version

--- app/src/main/java/com/spp/android/myapplication/StartSelection.kt
+++ app/src/main/java/com/spp/android/myapplication/StartSelection.kt
@@ -10,14 +10,14 @@ import androidx.lifecycle.lifecycleScope
 import com.spp.android.myapplication.data.UserPreferences
 import com.spp.android.myapplication.screens.contacts.ContactsScreen
 import com.spp.android.myapplication.ui.theme.MyApplicationTheme
-import com.spp.android.myapplication.xmlscreens.contacts.ContactsActivityXml
+//import com.spp.android.myapplication.xmlscreens.contacts.ContactsActivityXml
 import kotlinx.coroutines.launch
 
 class StartSelection : ComponentActivity() {
 
     companion object {
         /** If true – use Compose version, if false – XML version. */
-        const val USE_COMPOSE = false
+        const val USE_COMPOSE = true
         const val TEST_MODE = true
     }
 
@@ -58,7 +58,7 @@ class StartSelection : ComponentActivity() {
             }
         } else {
             //startActivity(Intent(this, LoginActivityXml::class.java))
-            startActivity(Intent(this, ContactsActivityXml::class.java))
+//            startActivity(Intent(this, ContactsActivityXml::class.java))
             finish()
         }
     }
