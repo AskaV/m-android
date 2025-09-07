3e82f30113736f0df736001fe4f27553e927380e
AskaV <caya2321@gmail.com>
2025-09-07 06:28:56 +0200

    cleanup files, gradle and libs, and add hilt to dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,12 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.safeArgs)
     alias(libs.plugins.kotlin.serialization)
-    id("org.jetbrains.kotlin.plugin.parcelize")
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt)
 }
-
+kapt {
+    correctErrorTypes = true
+}
 android {
     namespace = "com.spp.android.myapplication"
     compileSdk = 35
@@ -17,8 +20,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -31,57 +32,39 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
-    }
 }
 
 dependencies {
+
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.fragment)
-    implementation(libs.androidx.recyclerview)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.common)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.compose.compiler)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.material)
-    implementation(libs.glide)
-    implementation(libs.picasso)
-    implementation(libs.fresco)
-    implementation(libs.coil)
-    implementation(libs.gson)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
-    implementation(libs.coil.compose)
-    implementation (libs.accompanist.drawablepainter)
-    implementation(libs.kotlinx.serialization.json)
+
     implementation(libs.hilt.android)
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.lifecycle.runtime.compose)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+//    implementation(libs.glide)
+//    implementation(libs.picasso)
+//    implementation(libs.fresco)
+//    implementation(libs.coil)
+//    implementation(libs.coil.compose)
+
 }
