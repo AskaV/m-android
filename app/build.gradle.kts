540ef26b62a01ac54a90a5059a68fafe171e3b5a
AskaV <caya2321@gmail.com>
2026-02-08 02:19:21 +0100

    delete compose libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,9 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.safeArgs)
-    alias(libs.plugins.kotlin.serialization)
     id("org.jetbrains.kotlin.plugin.parcelize")
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
@@ -21,10 +18,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -45,19 +39,12 @@ tasks.named("check") {
 }
 
 ktlint {
-
     android.set(true)
     ignoreFailures.set(true)
     outputToConsole.set(true)
     verbose.set(true)
-    additionalEditorconfig.set(
-        mapOf(
-            "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
-        ),
-    )
 }
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.fragment)
@@ -65,32 +52,11 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.common)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.compose.compiler)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.material)
     implementation(libs.glide)
     implementation(libs.picasso)
     implementation(libs.fresco)
     implementation(libs.coil)
-    implementation(libs.gson)
     implementation(libs.androidx.datastore.preferences)
-    implementation(libs.coil.compose)
-    implementation(libs.cronet.embedded)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.kotlinx.serialization.json)
-    compileOnly(libs.infer.annotation)
 }
